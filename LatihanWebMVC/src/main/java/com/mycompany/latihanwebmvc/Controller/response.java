b2dd647ca17caaa6a259bb9628f2389ebf463231
Anak Agung Sagung Putri Wijayanti <138758788+sagungputri@users.noreply.github.com>
2024-12-12 01:47:34 +0700

    Add files via upload

---
+++ LatihanWebMVC/src/main/java/com/mycompany/latihanwebmvc/Controller/response.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.latihanwebmvc.Controller;
+
+/**
+ *
+ * @author gungp
+ */
+class response {
+
+    static void sendError(int SC_INTERNAL_SERVER_ERROR, String terjadi_kesalahan_dalam_memproses_request) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
